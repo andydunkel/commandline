81b69fd1e1faff06e44119a15c488249825bd09f
Andy Dunkel <a.dunkel@pilz.de>
2023-01-11 16:10:06 +0100

    Checkserver tool

---
+++ CheckServer/CheckServer/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace CheckServer;
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            PrintHelp();
+            return;
+        }
+        
+        MainAsync(args[0]).Wait();    
+    }
+    
+    static async Task MainAsync(string url)
+    {
+        var uri = new Uri(url, UriKind.Absolute);
+        
+        using var client = new HttpClient();
+        client.Timeout = new TimeSpan(0, 0, 5);
+
+        try
+        {
+            var result = await client.GetAsync(uri);
+            Console.WriteLine((int)result.StatusCode);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+        
+    }
+
+    private static void PrintHelp()
+    {
+        Console.WriteLine("Usage: CheckServer.exe https://mydomain.de");
+    }
+}
+
